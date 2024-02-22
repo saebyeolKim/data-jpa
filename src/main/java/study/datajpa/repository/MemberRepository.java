15036a8d65f99d52c19af4dcddcb2921982a0d1b
saebyeolKim <toquf2611@naver.com>
2024-02-22 13:37:47 +0900

    feat: @Query, 리포지토리 메소드에 쿼리 정의하기

--- src/main/java/study/datajpa/repository/MemberRepository.java
+++ src/main/java/study/datajpa/repository/MemberRepository.java
@@ -11,11 +11,14 @@ import java.util.List;
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
-    List<Member> findByUsernameAndAgeGreaterThan(String username, int age);
+    List<Member> findByUsernameAndAgeGreaterThan(String username, int age); //너무 길어져서 문제
     List<Member> findHelloBy();
 
     List<Member> findTop3HelloBy();
 
 //    @Query(name = "Member.findByUsername") 없어도 실행 됨
     List<Member> findByUsername(@Param("username") String username);
+
+    @Query("select m from Member m where m.username = :username and m.age = :age") //얘를 많이 사용
+    List<Member> findUser(@Param("username") String username, @Param("age") int age);
 }
